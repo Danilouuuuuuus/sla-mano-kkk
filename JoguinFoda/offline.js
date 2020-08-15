15232418ebca4185fc079bbc969c207175cd0029
Danilouuuuuuus <53249955+Danilouuuuuuus@users.noreply.github.com>
2020-08-15 04:45:55 -0300

    Add files via upload

---
+++ JoguinFoda/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1597476513,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/espinhos-sheet0.png",
+		"images/playeranimações-sheet0.png",
+		"images/tilesolido.png",
+		"images/camera-sheet0.png",
+		"images/npcsarahjbl-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
